05007d64e2b1173ed739e65b67f372d85e0069e8
Rennlyne Simiyu <st02503412024@students.ouk.ac.ke>
2025-08-20 11:00:23 +0300

    Add files via upload

---
+++ school.sql
@@ -0,0 +1,10 @@
+CREATE TABLE student (
+    name VARCHAR(100),
+    age INT,
+    gender VARCHAR(10)
+);
+
+INSERT INTO student (name, age, gender) VALUES
+('Alice Johnson', 20, 'Female'),
+('Brian Kim', 22, 'Male'),
+('Cynthia Lee', 19, 'Female');
